a71c89f841fa5b3023f007bdfe84be7f0d0b29f4
Saul Lee <solleedata@gmail.com>
2023-07-17 11:25:25 +0900

    feat: 앱을 WagmiConfig로 감싸기

--- src/app/providers.tsx
+++ src/app/providers.tsx
@@ -1,12 +1,14 @@
-'use client'
+'use client';
 
-import * as React from 'react'
-import { WagmiConfig } from 'wagmi'
+import * as React from 'react';
+import { WagmiConfig } from 'wagmi';
 
-import { config } from '../wagmi'
+import { config } from '../wagmi';
 
 export function Providers({ children }: { children: React.ReactNode }) {
-  const [mounted, setMounted] = React.useState(false)
-  React.useEffect(() => setMounted(true), [])
-  return <WagmiConfig config={config}>{mounted && children}</WagmiConfig>
+  const [mounted, setMounted] = React.useState(false);
+  React.useEffect(() => setMounted(true), []);
+
+  /** WagmiConfig로 app을 감싸기: */
+  return <WagmiConfig config={config}>{mounted && children}</WagmiConfig>;
 }
